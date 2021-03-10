9034d049a01791abcbe5074b61a2e86661d67668
David <dmarquez.estudis@gmail.com>
2021-03-10 16:43:50 +0100

    fix(date): Fix date format wasn't working as expected

--- __test__/formatters/cells/generatorDateCell.spec.js
+++ __test__/formatters/cells/generatorDateCell.spec.js
@@ -1,9 +1,10 @@
 import generatorDateCell from '../../../src/formatters/cells/generatorDateCell';
 
-export const expectedXML = '<c r="A1" s="1"><v>36501</v></c>';
+export const expectedXML = '<c r="A1" s="1"><v>36504</v></c>';
 
 describe('Cell of type Date', () => {
   it('Should create a new xml markup cell', () => {
-    expect(generatorDateCell(0, new Date(1999, 12, 10), 1, 1)).toBe(expectedXML);
+    //Date is 10/12/1999
+    expect(generatorDateCell(0, new Date(1999, 11, 10), 1, 1)).toBe(expectedXML);
   });
 });
